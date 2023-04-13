61ca409c214630f3a87e177584ea1776212a4f7c
baegteun <baegteun@gmail.com>
2023-04-14 08:15:39 +0900

    :sparkles: :: SMS Endpoint

--- Projects/Domain/BaseDomain/Sources/Base/SMSEndpoint.swift
+++ Projects/Domain/BaseDomain/Sources/Base/SMSEndpoint.swift
@@ -1,5 +1,17 @@
 import Emdpoint
+import Foundation
 
-protocol SMSEndpoint: EndpointType {
-    
+public protocol SMSEndpoint: EndpointType, JwtAuthorizable {
+    associatedtype ErrorType: Error
+    var errorMap: [Int: ErrorType] { get }
+}
+
+extension SMSEndpoint {
+    public var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+
+    public var validationCode: ClosedRange<Int> {
+        200...299
+    }
 }
