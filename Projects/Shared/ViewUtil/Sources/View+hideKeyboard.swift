98f1f49b45d934652132497584b2783bc076d1e3
baegteun <baegteun@gmail.com>
2023-04-15 21:08:07 +0900

    :sparkles: :: View + hideKeyboard

---
+++ Projects/Shared/ViewUtil/Sources/View+hideKeyboard.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil,
+            from: nil,
+            for: nil
+        )
+    }
+}
