c12aace90801750279207ac801b2656b6a1aaf53
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-30 22:15:55 +0900

    :iphone: SMSImage 제작

--- Projects/Core/DesignSystem/Sources/Image/SMSImage.swift
+++ Projects/Core/DesignSystem/Sources/Image/SMSImage.swift
@@ -1,9 +1,37 @@
-//
-//  SMSImage.swift
-//  DesignSystem
-//
-//  Created by sunghun on 2023/04/25.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+fileprivate extension DesignSystemImages {
+    var suiImage: SwiftUI.Image {
+        SwiftUI.Image(uiImage: self.image)
+    }
+}
+
+public struct SMSImage: View {
+    public enum Image {
+        case background
+    }
+
+    private var image: Image
+    private var renderingMode: SwiftUI.Image.TemplateRenderingMode
+
+    public init(
+        _ image: Image,
+        renderingMode: SwiftUI.Image.TemplateRenderingMode = .original
+    ) {
+        self.image = image
+        self.renderingMode = renderingMode
+    }
+
+    public var body: some View {
+        smsImageToImage()
+            .resizable()
+            .renderingMode(renderingMode)
+    }
+
+    private func smsImageToImage() -> SwiftUI.Image {
+        switch image {
+        case .background:
+            return DesignSystemAsset.Images.background.suiImage
+        }
+    }
+}
