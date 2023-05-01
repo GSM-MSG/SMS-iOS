17739d6c2c76c8ac8e6dbe3799a8c83e4d2a625e
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-01 17:34:31 +0900

    Update Projects/Feature/SigninFeature/Project.swift
    Co-authored-by: baegteun <baegteun@gmail.com>

--- Projects/Feature/SigninFeature/Project.swift
+++ Projects/Feature/SigninFeature/Project.swift
@@ -12,9 +12,5 @@ let project = Project.makeModule(
     internalDependencies: [
         .Domain.AuthDomainInterface,
         .Feature.BaseFeature
-    ],
-    additionalPlistRows: [
-        "CLIENT_ID": .string("$(CLIENT_ID)"),
-        "REDIREDCT_URI": .string("$(REDIREDCT_URI)")
     ]
 )
