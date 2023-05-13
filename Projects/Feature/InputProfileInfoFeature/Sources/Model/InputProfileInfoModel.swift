8bd030a0ec2f4c83a37450c1ac3f8150cad55f68
baegteun <baegteun@gmail.com>
2023-05-13 15:08:43 +0900

    :sparkles: :: InputProfileInfoModel

---
+++ Projects/Feature/InputProfileInfoFeature/Sources/Model/InputProfileInfoModel.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+final class InputProfileInfoModel: InputProfileInfoStateProtocol {}
+
+extension InputProfileInfoModel: InputProfileInfoActionProtocol {}
---
+++ Projects/Feature/InputProfileInfoFeature/Sources/Model/InputProfileInfoModelProtocol.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol InputProfileInfoStateProtocol {}
+
+protocol InputProfileInfoActionProtocol {}
