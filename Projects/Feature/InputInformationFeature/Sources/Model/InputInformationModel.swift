9eff456e3c9658a431407f4106bed5ed3aa06fa9
baegteun <baegteun@gmail.com>
2023-05-13 13:23:13 +0900

    :sparkles: :: InputInformationModel Imple

---
+++ Projects/Feature/InputInformationFeature/Sources/Model/InputInformationModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+final class InputInformationModel: ObservableObject, InputInformationStateProtocol {
+    @Published var phase: InformationPhase = .profile
+}
+
+extension InputInformationModel: InputInformationActionProtocol {
+    func prevButtonDidTap() {
+        guard let currentIndex = InformationPhase.allCases.firstIndex(of: phase),
+              currentIndex > 1
+        else { return }
+        phase = InformationPhase.allCases[currentIndex - 1]
+    }
+
+    func nextButtonDidTap() {
+        guard let currentIndex = InformationPhase.allCases.firstIndex(of: phase),
+              currentIndex < InformationPhase.allCases.count - 1
+        else { return }
+        phase = InformationPhase.allCases[currentIndex + 1]
+    }
+}
