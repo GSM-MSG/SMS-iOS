3c7eb48a440342768004bf2992e36d9212ad0aa6
baegteun <baegteun@gmail.com>
2023-05-13 15:05:32 +0900

    :sparkles: :: InputProfileInfoView Scaffold

---
+++ Projects/Feature/InputProfileInfoFeature/Sources/Scene/InputProfileInfoView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct InputProfileInfoView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct InputProfileInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputProfileInfoView()
+    }
+}
