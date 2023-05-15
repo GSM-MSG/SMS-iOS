1604ae67ebd60dd82cf76f924f8c15ec56ab4220
baegteun <baegteun@gmail.com>
2023-05-15 17:29:58 +0900

    :sparkles: :: Input Profile Info State

--- Projects/Feature/InputProfileInfoFeature/Sources/Model/InputProfileInfoModel.swift
+++ Projects/Feature/InputProfileInfoFeature/Sources/Model/InputProfileInfoModel.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-final class InputProfileInfoModel: ObservableObject, InputProfileInfoStateProtocol {}
+final class InputProfileInfoModel: ObservableObject, InputProfileInfoStateProtocol {
+    @Published var introduce: String = ""
+    @Published var email: String = ""
+    @Published var major: String = ""
+    @Published var portfoiloURL: String = ""
+    @Published var techStack: String = ""
+}
 
 extension InputProfileInfoModel: InputProfileInfoActionProtocol {}
--- Projects/Feature/InputProfileInfoFeature/Sources/Model/InputProfileInfoModelProtocol.swift
+++ Projects/Feature/InputProfileInfoFeature/Sources/Model/InputProfileInfoModelProtocol.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-protocol InputProfileInfoStateProtocol {}
+protocol InputProfileInfoStateProtocol {
+    var introduce: String { get }
+    var email: String { get }
+    var major: String { get }
+    var portfoiloURL: String { get }
+    var techStack: String { get }
+}
 
 protocol InputProfileInfoActionProtocol: AnyObject {}
