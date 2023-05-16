ba55d90e1a646f08fb0bdde89726ff762fcb8b29
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-17 01:14:39 +0900

    :sparkles: StudentDomain Interface Repository

--- Projects/Domain/StudentDomain/Interface/Repository/StudentRepository.swift
+++ Projects/Domain/StudentDomain/Interface/Repository/StudentRepository.swift
@@ -1,9 +1,5 @@
-//
-//  StudentRepository.swift
-//  StudentDomainInterface
-//
-//  Created by sunghun on 2023/05/16.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
-
 import Foundation
+
+public protocol StudentRepository {
+    func inputInformation(req: StudentRequestDTO) async throws
+}
