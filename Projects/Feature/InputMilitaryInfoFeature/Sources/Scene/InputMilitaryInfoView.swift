cf80792f978d78fb83299cac9b78710494b56226
baegteun <baegteun@gmail.com>
2023-05-17 08:26:41 +0900

    :lipstick: :: 병역 정보 입력 페이지 UI

--- Projects/Feature/InputMilitaryInfoFeature/Sources/Scene/InputMilitaryInfoView.swift
+++ Projects/Feature/InputMilitaryInfoFeature/Sources/Scene/InputMilitaryInfoView.swift
@@ -1,4 +1,5 @@
 import BaseFeature
+import DesignSystem
 import SwiftUI
 
 struct InputMilitaryInfoView: View {
@@ -7,6 +8,55 @@ struct InputMilitaryInfoView: View {
     var state: any InputMilitaryInfoStateProtocol { container.model }
 
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { proxy in
+            SMSNavigationTitleView(title: "정보입력") {
+                Rectangle()
+                    .fill(Color.sms(.neutral(.n10)))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 16)
+
+                VStack(spacing: 32) {
+                    pageTitleView()
+
+                    VStack(spacing: 24) {
+                        SMSTextField("병특 희망", text: .constant(""))
+                            .disabled(true)
+                            .overlay(alignment: .trailing) {
+                                SMSIcon(.downChevron)
+                                    .padding(.trailing, 12)
+                            }
+                            .titleWrapper("병특 희망 여부")
+                    }
+
+                    Spacer()
+
+                    HStack(spacing: 8) {
+                        CTAButton(text: "이전", style: .outline)
+                            .frame(maxWidth: proxy.size.width / 3)
+
+                        CTAButton(text: "다음")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .padding(.bottom, 32)
+                }
+                .padding([.top, .horizontal], 20)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func pageTitleView() -> some View {
+        HStack(spacing: 4) {
+            Text("병역")
+                .foregroundColor(.sms(.system(.black)))
+
+            Text("*")
+                .foregroundColor(.sms(.sub(.s2)))
+
+            Spacer()
+
+            SMSPageControl(pageCount: 6, selectedPage: 3)
+        }
+        .smsFont(.title1)
     }
 }
