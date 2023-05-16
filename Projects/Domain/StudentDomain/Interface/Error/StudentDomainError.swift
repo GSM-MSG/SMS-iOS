eba1335f8d3249a6a10ac7ad075bcacbc17856ae
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-17 01:14:12 +0900

    :sparkles: StudentDomain Interface Error

--- Projects/Domain/StudentDomain/Interface/Error/StudentDomainError.swift
+++ Projects/Domain/StudentDomain/Interface/Error/StudentDomainError.swift
@@ -1,9 +1,18 @@
-//
-//  StudentDomainError.swift
-//  StudentDomainInterface
-//
-//  Created by sunghun on 2023/05/16.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
-
 import Foundation
+
+public enum StudentDomainError: Error {
+    case invalidGAuthCode
+    case internalServerError
+}
+
+extension StudentDomainError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidGAuthCode:
+            return "Request 요청 값 다름"
+
+        case .internalServerError:
+            return "이미 존재함"
+        }
+    }
+}
