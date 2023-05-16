9cdaca5c4c4be0aac852c1dc361a9914d99415a8
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-17 01:39:50 +0900

    :sparkles: StudentDomain Endpoing

--- Projects/Domain/StudentDomain/Sources/Endpoint/StudentEndpoint.swift
+++ Projects/Domain/StudentDomain/Sources/Endpoint/StudentEndpoint.swift
@@ -1,9 +1,59 @@
-//
-//  StudentEndpoint.swift
-//  StudentDomainInterface
-//
-//  Created by sunghun on 2023/05/16.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
+import BaseDomain
+import Emdpoint
+import StudentDomainInterface
 
-import Foundation
+enum StudentEndpoint {
+    case inputInformation(StudentRequestDTO)
+}
+
+extension StudentEndpoint: SMSEndpoint {
+    typealias ErrorType = StudentDomainError
+
+    var route: Route {
+        switch self {
+        case .inputInformation:
+            return .post("/student")
+        }
+    }
+
+    var task: HTTPTask {
+        switch self {
+        case let .inputInformation(req):
+            return .requestParameters(body: [
+                "major": req.major,
+                "techStack": req.techStack,
+                "profileImgUrl": req.profileImgUrl,
+                "introduce": req.introduce,
+                "stuNum": req.stuNum,
+                "portfolioUrl": req.portfolioUrl,
+                "contactEmail": req.contactEmail,
+                "gsmAuthenticationScore": req.gsmAuthenticationScore,
+                "salary": req.salary,
+                "region": req.region,
+                "languageCertificate": req.languageCertificate,
+                "dreamBookFileUrl": req.dreamBookFileUrl,
+                "militaryService": req.militaryService,
+                "certificate": req.certificate
+            ])
+
+        default:
+            return .requestPlain
+        }
+    }
+
+    var jwtTokenType: JwtTokenType {
+        switch self {
+        default:
+            return .accessToken
+        }
+    }
+
+    var errorMap: [Int: ErrorType]? {
+        switch self {
+        case .inputInformation:
+            return [
+                500: .internalServerError
+            ]
+        }
+    }
+}
