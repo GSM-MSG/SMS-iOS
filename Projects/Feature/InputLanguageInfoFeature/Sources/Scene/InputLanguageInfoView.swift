04cf2e03cb3cad8e87b391f3820983b3a44c3ae4
baegteun <baegteun@gmail.com>
2023-05-18 09:34:14 +0900

    :lipstick: :: InputLanguageInfoView

--- Projects/Feature/InputLanguageInfoFeature/Sources/Scene/InputLanguageInfoView.swift
+++ Projects/Feature/InputLanguageInfoFeature/Sources/Scene/InputLanguageInfoView.swift
@@ -1,5 +1,7 @@
 import BaseFeature
+import DesignSystem
 import SwiftUI
+import ViewUtil
 
 struct InputLanguageInfoView: View {
     @StateObject var container: MVIContainer<InputLanguageInfoIntentProtocol, InputLanguageInfoStateProtocol>
@@ -7,6 +9,60 @@ struct InputLanguageInfoView: View {
     var state: any InputLanguageInfoStateProtocol { container.model }
 
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { proxy in
+            SMSNavigationTitleView(title: "정보입력") {
+                Rectangle()
+                    .fill(Color.sms(.neutral(.n10)))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 16)
+
+                VStack(spacing: 32) {
+                    pageTitleView()
+
+                    VStack(spacing: 8) {
+                        languageListView(proxy: proxy)
+                            .titleWrapper("외국어")
+                            .aligned(.leading)
+
+                        SMSChip("추가")
+                            .aligned(.leading)
+                    }
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    func pageTitleView() -> some View {
+        HStack(spacing: 4) {
+            Text("외국어")
+                .foregroundColor(.sms(.system(.black)))
+            
+            Text("*")
+                .foregroundColor(.sms(.sub(.s2)))
+            
+            Spacer()
+            
+            SMSPageControl(pageCount: 6, selectedPage: 4)
+        }
+        .smsFont(.title1)
+    }
+
+    @ViewBuilder
+    func languageListView(proxy: GeometryProxy) -> some View {
+        VStack(spacing: 12) {
+            ForEach(1..<5, id: \.self) { index in
+                HStack(spacing: 16) {
+                    SMSTextField("예) 토익", text: .constant(""))
+                        .frame(maxWidth: .infinity)
+
+                    SMSTextField("900", text: .constant(""))
+                        .frame(maxWidth: proxy.size.width / 3)
+
+                    Image(systemName: "trash")
+                        .frame(width: 24, height: 24)
+                }
+            }
+        }
     }
 }
