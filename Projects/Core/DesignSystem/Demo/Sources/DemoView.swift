f326a31c5d9dddf370c8747e8d64dbaa444175aa
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-22 17:52:41 +0900

    :boom: 결국 해결책을 찾지 못하고 처음으로 돌아왔습니다

--- Projects/Core/DesignSystem/Demo/Sources/DemoView.swift
+++ Projects/Core/DesignSystem/Demo/Sources/DemoView.swift
@@ -2,22 +2,19 @@ import DesignSystem
 import SwiftUI
 
 public struct DemoView: View {
-    @State var isOn = true
+    @State var isSeleted = true
     @State var selection: Int? = 0
 
     public var body: some View {
         VStack {
             Group {
-                RadioComponent(selection: $selection) {
                     ForEach(0..<4) { index in
                         HStack {
-                            SMSSelectionControls(tag: index)
-                                .radioTag(index)
+                            SMSSelectionControls(isSeleted: $isSeleted)
                             Text("asdfasdf")
                         }
                     }
                 }
-            }
         }
     }
 }
--- Projects/Core/DesignSystem/Sources/SelectionControls/SMSSelectionControls.swift
+++ Projects/Core/DesignSystem/Sources/SelectionControls/SMSSelectionControls.swift
@@ -1,22 +1,42 @@
 import SwiftUI
 
-public struct SMSSelectionControls<T: Hashable>: View {
-    @EnvironmentObject private var value: RadioValue<T>
-    private var tag: T?
+public struct SMSSelectionControls: View {
+    @Binding var isSeleted: Bool
 
-    public init(tag: T?) {
-        self.tag = tag
+    public init(isSeleted: Binding<Bool>) {
+        _isSeleted = isSeleted
+    }
+    public var body: some View {
+        Button {
+            self.isSeleted.toggle()
+        } label: {
+            ZStack {
+                Circle()
+                    .stroke(isSeleted ? Color.sms(.primary(.p2)) : Color.sms(.neutral(.n20)), lineWidth: 2)
+                    .frame(width: 20, height: 20)
+                Circle()
+                    .fill(isSeleted ? Color.sms(.primary(.p2)) : .clear)
+                    .frame(width: 12, height: 12)
+            }
+        }
+        .buttonStyle(CustomButtonStyle(isSeleted: isSeleted))
+    }
+}
+
+private struct CustomButtonStyle: ButtonStyle {
+    var isSeleted: Bool
+
+    init(isSeleted: Bool) {
+        self.isSeleted = isSeleted
     }
 
-    public var body: some View {
-        ZStack {
-            Circle()
-                .stroke(tag == value.selection ? Color.sms(.primary(.p2)) : Color.sms(.neutral(.n20)), lineWidth: 2)
-                .frame(width: 20, height: 20)
-
-            Circle()
-                .fill(tag == value.selection ? Color.sms(.primary(.p2)) : .clear)
-                .frame(width: 12, height: 12)
-        }
+    @ViewBuilder
+    func makeBody(configuration: Configuration) -> some View {
+        Circle()
+            .fill(self.isSeleted && configuration.isPressed ? Color.sms(.neutral(.n20)) : Color.clear)
+            .overlay {
+                configuration.label
+            }
+            .frame(width: 32, height: 32)
     }
 }
