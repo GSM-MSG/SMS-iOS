d9109dd51f1bad7ff6960a6948d71030e50a5158
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-23 00:05:49 +0900

    :sparkles: MajorEndpoint

---
+++ Projects/Domain/MajorDomain/Sources/Endpoint/MajorEndpoint.swift
@@ -0,0 +1,47 @@
+import AuthDomainInterface
+import BaseDomain
+import Emdpoint
+
+enum MajorEndpoint {
+    case fetchList
+}
+
+extension MajorEndpoint: SMSEndpoint {
+    typealias ErrorType = AuthDomainError
+
+    var route: Route {
+        switch self {
+        case .fetchList:
+            return .post("/list")
+        }
+    }
+
+    var task: HTTPTask {
+        switch self {
+        case .fetchList:
+            return .requestPlain
+
+        default:
+            return .requestPlain
+        }
+    }
+
+    var jwtTokenType: JwtTokenType {
+        switch self {
+        case .fetchList:
+            return .accessToken
+
+        default:
+            return .none
+        }
+    }
+
+    var errorMap: [Int: ErrorType]? {
+        switch self {
+        case .fetchList:
+            return [
+                500: .internalServerError
+            ]
+        }
+    }
+}
