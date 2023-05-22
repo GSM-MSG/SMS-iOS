400f52d00e290e661c251f579980be21f1c72c42
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-22 22:52:38 +0900

    :sparkles: FileEndPoint

--- Projects/Domain/FileDomain/Sources/Endpoint/FileEndpoint.swift
+++ Projects/Domain/FileDomain/Sources/Endpoint/FileEndpoint.swift
@@ -1,9 +1,49 @@
-//
-//  FileEndpoint.swift
-//  FileDomainInterface
-//
-//  Created by sunghun on 2023/05/22.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
-
+import FileDomainInterface
 import Foundation
+import Emdpoint
+import BaseDomain
+
+enum FileEndpoint {
+    case dreamBookUpload(file: Data)
+}
+
+extension FileEndpoint: SMSEndpoint {
+    typealias ErrorType = FileDomainError
+
+    var route: Route {
+        switch self {
+        case .dreamBookUpload:
+            return .post("/")
+        }
+    }
+
+    var task: HTTPTask {
+        switch self {
+        case let .dreamBookUpload(file):
+            return .uploadMultipart([
+                MultiPartFormData(field: "file", data: file)
+            ])
+
+        default:
+            return .requestPlain
+        }
+    }
+
+    var jwtTokenType: JwtTokenType {
+        switch self {
+        case .dreamBookUpload:
+            return .accessToken
+        default:
+            return .none
+        }
+    }
+
+    var errorMap: [Int: ErrorType]? {
+        switch self {
+        case .dreamBookUpload:
+            return [
+                500: .internalServerError
+            ]
+        }
+    }
+}
