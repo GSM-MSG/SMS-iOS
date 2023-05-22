3bc4be94b3b5e886f13b0c217ded2dd007c647ba
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-22 22:56:42 +0900

    :sparkles: FileDomain

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/ModulePaths.swift
@@ -29,6 +29,7 @@ public extension ModulePaths {
 
 public extension ModulePaths {
     enum Domain: String {
+        case FileDomain
         case StudentDomain
         case AuthDomain
         case BaseDomain
