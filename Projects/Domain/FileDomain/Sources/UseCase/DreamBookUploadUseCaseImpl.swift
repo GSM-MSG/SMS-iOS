808aa3661fc901dcfe5c2ff94569852b26d01a12
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-22 22:53:17 +0900

    :sparkles: DreamBookUploadUseCaseIMpl

--- Projects/Domain/FileDomain/Sources/UseCase/DreamBookUploadUseCaseImpl.swift
+++ Projects/Domain/FileDomain/Sources/UseCase/DreamBookUploadUseCaseImpl.swift
@@ -1,9 +1,14 @@
-//
-//  DreamBookUploadUseCase.swift
-//  FileDomainInterface
-//
-//  Created by sunghun on 2023/05/22.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
-
+import FileDomainInterface
 import Foundation
+
+struct DreamBookUploadUseCaseImpl: DreamBookUploadUseCase {
+    private let fileRepository: any FileRepository
+
+    init(fileRepository: any FileRepository) {
+        self.fileRepository = fileRepository
+    }
+
+    func execute(file: Data) async throws -> String {
+        try await fileRepository.dreamBookUpload(file: file)
+    }
+}
