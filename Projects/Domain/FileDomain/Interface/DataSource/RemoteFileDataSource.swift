857f66b71d6ef67946a2bdb236d7b7e136dde254
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-23 15:35:25 +0900

    :sparkles: RemoteFileDataSource

--- Projects/Domain/FileDomain/Interface/DataSource/RemoteFileDataSource.swift
+++ Projects/Domain/FileDomain/Interface/DataSource/RemoteFileDataSource.swift
@@ -2,4 +2,5 @@ import Foundation
 
 public protocol RemoteFileDataSource {
     func dreamBookUpload(file: Data, fileName: String) async throws -> String
+    func imageUpload(image: Data, fileName: String) async throws -> String
 }
