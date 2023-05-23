b1a7fa673cc9443a533c0b162b0d9f20474b4441
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-23 15:36:07 +0900

    :sparkles: ImageUploadUseCase

--- Projects/Domain/FileDomain/Interface/UseCase/ImageUploadUseCase.swift
+++ Projects/Domain/FileDomain/Interface/UseCase/ImageUploadUseCase.swift
@@ -1,9 +1,5 @@
-//
-//  ImageUploadUseCase.swift
-//  FileDomainInterface
-//
-//  Created by sunghun on 2023/05/23.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
-
 import Foundation
+
+public protocol ImageUploadUseCase {
+    func execute(image: Data, fileName: String) async throws -> String
+}
