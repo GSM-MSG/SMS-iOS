f50ebd69ed28e8f3801d76f2a70dc622857b414a
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-05-23 15:36:45 +0900

    :sparkles: ImageUploadResponseDTO

--- Projects/Domain/FileDomain/Sources/DTO/Response/ImageUploadResponseDTO.swift
+++ Projects/Domain/FileDomain/Sources/DTO/Response/ImageUploadResponseDTO.swift
@@ -1,9 +1,9 @@
-//
-//  ImageUploadResponseDTO.swift
-//  FileDomainInterface
-//
-//  Created by sunghun on 2023/05/23.
-//  Copyright © 2023 com.msg. All rights reserved.
-//
-
 import Foundation
+
+public struct ImageUploadResponseDTO: Decodable {
+    public let fileURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case fileURL = "fileUrl"
+    }
+}
