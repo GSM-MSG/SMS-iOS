b61a6e8a8afd9cbc95416e967c9aabaa21e0f365
baegteun <baegteun@gmail.com>
2023-05-26 23:16:18 +0900

    :lipstick: :: TextField에 텍스트 입력 시 xmark와 겹쳐지던 현상 해결

--- Projects/Core/DesignSystem/Demo/Sources/DemoView.swift
+++ Projects/Core/DesignSystem/Demo/Sources/DemoView.swift
@@ -2,18 +2,12 @@ import DesignSystem
 import SwiftUI
 
 public struct DemoView: View {
-    @State var isSeleted = true
-    @State var selection: Int? = 0
+    @State var text = ""
 
     public var body: some View {
         VStack {
             Group {
-                ForEach(0..<4) { index in
-                    HStack {
-                        SMSSelectionControls(isSeleted: $isSeleted)
-                        Text("asdfasdf")
-                    }
-                }
+                SMSTextField("ASDF", text: $text)
             }
         }
     }
--- Projects/Core/DesignSystem/Sources/TextField/SMSTextField.swift
+++ Projects/Core/DesignSystem/Sources/TextField/SMSTextField.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import ViewUtil
 
 public struct SMSTextField: View {
     @Binding var text: String
@@ -27,47 +28,36 @@ public struct SMSTextField: View {
 
     public var body: some View {
         VStack(alignment: .leading, spacing: 8) {
-            TextField(placeholder, text: $text)
-                .onSubmit(onSubmit)
-                .smsFont(.body1, color: .neutral(.n50))
-                .padding(.horizontal, 12)
-                .padding(.vertical, 13.5)
-                .focused($isFocused)
-                .background(Color.sms(.neutral(.n10)))
-                .cornerRadius(8)
-                .overlay {
-                    if isFocused {
-                        RoundedRectangle(cornerRadius: 8)
-                            .strokeBorder(Color.sms(.primary(.p2)))
-                    }
+            HStack {
+                TextField(placeholder, text: $text)
+                    .onSubmit(onSubmit)
+                    .smsFont(.body1, color: .neutral(.n50))
+                    .focused($isFocused)
+
+                ConditionView(!text.isEmpty && isOnClear) {
+                    SMSIcon(.xmark)
+                        .buttonWrapper {
+                            text = ""
+                        }
                 }
-                .if(isOnClear) {
-                    $0.modifier(SMSTextFieldClearModifier(text: $text))
-                }
+            }
+            .padding(.horizontal, 12)
+            .frame(height: 48)
+            .background(Color.sms(.neutral(.n10)))
+            .cornerRadius(8)
+            .overlay {
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(Color.sms(.primary(.p2)))
+                    .conditional(isFocused)
+            }
+            .onTapGesture {
+                isFocused = true
+            }
 
             if isError && !errorText.isEmpty {
                 Text(errorText)
                     .padding(.leading, 8)
                     .smsFont(.caption1, color: .system(.error))
-            }
-        }
-    }
-}
-
-struct SMSTextFieldClearModifier: ViewModifier {
-    @Binding var text: String
-
-    func body(content: Content) -> some View {
-        ZStack(alignment: .trailing) {
-            content
-
-            if !text.isEmpty {
-                Button {
-                    self.text = ""
-                } label: {
-                    SMSIcon(.xmark)
-                }
-                .padding(.trailing, 12)
             }
         }
     }
