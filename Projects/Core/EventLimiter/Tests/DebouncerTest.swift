a805842a88f57efc71b5160194d9bee0fb2284eb
baegteun <baegteun@gmail.com>
2023-06-08 20:25:03 +0900

    :fire: :: 실험용 Test 코드 주석화

--- Projects/Core/EventLimiter/Tests/DebouncerTest.swift
+++ Projects/Core/EventLimiter/Tests/DebouncerTest.swift
@@ -7,30 +7,30 @@ final class DebouncerTests: XCTestCase {
     override func tearDownWithError() throws {}
 
     func testExample() async {
-        let throttler = Throttler(for: 1)
-
-        throttler {
-            print("ASDf")
-        }
-        throttler {
-            print("ASDf2")
-        }
-
-        try? await Task.sleep(nanoseconds: 500_000_000)
-        throttler {
-            print("ASDf3")
-        }
-
-        try? await Task.sleep(nanoseconds: 600_000_000)
-        throttler {
-            print("ASDf4")
-        }
-
-        try? await Task.sleep(nanoseconds: 200_000_000)
-        throttler {
-            print("ASDf5")
-        }
-
-        try? await Task.sleep(nanoseconds: 5_000_000_000)
+//        let throttler = Throttler(for: 1)
+//
+//        throttler {
+//            print("ASDf")
+//        }
+//        throttler {
+//            print("ASDf2")
+//        }
+//
+//        try? await Task.sleep(nanoseconds: 500_000_000)
+//        throttler {
+//            print("ASDf3")
+//        }
+//
+//        try? await Task.sleep(nanoseconds: 600_000_000)
+//        throttler {
+//            print("ASDf4")
+//        }
+//
+//        try? await Task.sleep(nanoseconds: 200_000_000)
+//        throttler {
+//            print("ASDf5")
+//        }
+//
+//        try? await Task.sleep(nanoseconds: 5_000_000_000)
     }
 }
