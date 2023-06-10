bb1062ed966dbd5ca00070c7127faa16994e05c6
baegteun <baegteun@gmail.com>
2023-06-10 22:48:05 +0900

    :sparkles: :: API 수정사항 Test Double에도 적용

--- Projects/Domain/AuthDomain/Testing/Repository/AuthRepositorySpy.swift
+++ Projects/Domain/AuthDomain/Testing/Repository/AuthRepositorySpy.swift
@@ -2,11 +2,11 @@ import AuthDomainInterface
 
 final class AuthRepositorySpy: AuthRepository {
     var loginCallCount = 0
-    func login(code: String) async throws -> IsAlreadySignUp {
+    func login(code: String) async throws -> UserSignupInfoEntity {
         loginCallCount += 1
         if code.isEmpty {
             throw AuthDomainError.failedToGAuthSignin
         }
-        return true
+        return .init(isAlreadySignup: true, userRole: .guest)
     }
 }
