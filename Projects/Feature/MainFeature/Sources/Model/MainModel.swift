3b736b5aef33358cb48fc97a7dc3ef0c31f97a2a
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-06-12 17:09:51 +0900

    :sparkles: Model 가져와야할 요소 추가

--- Projects/Feature/MainFeature/Sources/Model/MainModel.swift
+++ Projects/Feature/MainFeature/Sources/Model/MainModel.swift
@@ -1,8 +1,14 @@
 import Foundation
+import StudentDomainInterface
 
 final class MainModel: ObservableObject, MainStateProtocol {
+    @Published var page: Int = 1
+    @Published var totalSize: Int = 0
+    @Published var isLast: Bool = false
     @Published var isError: Bool = false
     @Published var isSuccess: Bool = false
+    @Published var content: [SingleStudentEntity] =  []
+    @Published var studentList: PagedStudentListEntity = .init(studentList: [], totalSize: 0, isLast: false)
 }
 
 extension MainModel: MainActionProtocol {
@@ -12,5 +18,21 @@ extension MainModel: MainActionProtocol {
 
     func updateIsSuccess(isSuccess: Bool) {
         self.isSuccess = isSuccess
+    }
+
+    func updatePage(page: Int) {
+        self.page = page
+    }
+
+    func updateTotalSize(totalSize: Int) {
+        self.totalSize = totalSize
+    }
+
+    func updateIsLast(isLast: Bool) {
+        self.isLast = isLast
+    }
+
+    func appendContent(content: [SingleStudentEntity]) {
+        self.content.append(contentsOf: content)
     }
 }
--- Projects/Feature/MainFeature/Sources/Model/MainModelProtocol.swift
+++ Projects/Feature/MainFeature/Sources/Model/MainModelProtocol.swift
@@ -1,9 +1,19 @@
+import StudentDomainInterface
+
 protocol MainStateProtocol {
+    var page: Int { get }
+    var totalSize: Int { get }
+    var isLast: Bool { get }
     var isError: Bool { get }
     var isSuccess: Bool { get }
+    var content: [SingleStudentEntity] { get }
 }
 
 protocol MainActionProtocol: AnyObject {
     func updateIsError(isError: Bool)
     func updateIsSuccess(isSuccess: Bool)
+    func updatePage(page: Int)
+    func updateTotalSize(totalSize: Int)
+    func updateIsLast(isLast: Bool)
+    func appendContent(content: [SingleStudentEntity])
 }
