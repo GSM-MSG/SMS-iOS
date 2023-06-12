c49af96aae9730d5c51c2660d3a3ae114fe0a661
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-06-12 17:23:11 +0900

    Update Projects/Domain/StudentDomain/Sources/Endpoint/StudentEndpoint.swift
    Co-authored-by: baegteun <baegteun@gmail.com>

--- Projects/Domain/StudentDomain/Sources/Endpoint/StudentEndpoint.swift
+++ Projects/Domain/StudentDomain/Sources/Endpoint/StudentEndpoint.swift
@@ -30,9 +30,6 @@ extension StudentEndpoint: SMSEndpoint {
             return .requestJSONEncodable(req)
 
         case let .fetchStudentList(request):
-            print(request.page)
-            print(request.size)
-//            return .requestParameters(body: <#T##[String : Any]?#>, query: <#T##[String : Any]?#>)
             return .requestParameters(query: [
                 "page": "\(request.page)",
                 "size": "\(request.size)"
