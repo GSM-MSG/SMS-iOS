b565f64d3370426d9b41f03cc3648fc867721b71
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-07-04 17:52:05 +0900

    :sparkles: FilterTechStack model

--- Projects/Feature/FilterFeature/Sources/Model/FilterModel.swift
+++ Projects/Feature/FilterFeature/Sources/Model/FilterModel.swift
@@ -1,8 +1,12 @@
 import Foundation
+import UserDomainInterface
 
 final class FilterModel: ObservableObject, FilterStateProtocol {
+    @Published var userRole: UserRoleType = .guest
     @Published var isError: Bool = false
-    @Published var isSuccess: Bool = false
+    @Published var isPresentedTechStackAppend: Bool = false
+    @Published var techStacks: [String] = []
+    @Published var isLoading: Bool = false
 }
 
 extension FilterModel: FilterActionProtocol {
@@ -10,7 +14,19 @@ extension FilterModel: FilterActionProtocol {
         self.isError = isError
     }
 
-    func updateIsSuccess(isSuccess: Bool) {
-        self.isSuccess = isSuccess
+    func updateIsPresentedTeckStackAppend(isPresented: Bool) {
+        self.isPresentedTechStackAppend = isPresented
+    }
+
+    func updateTeckStacks(techStacks: [String]) {
+        self.techStacks = techStacks
+    }
+
+    func updateIsLoading(isLoading: Bool) {
+        self.isLoading = isLoading
+    }
+
+    func updateUserRole(role: UserRoleType) {
+        self.userRole = role
     }
 }
--- Projects/Feature/FilterFeature/Sources/Model/FilterModelProcool.swift
+++ Projects/Feature/FilterFeature/Sources/Model/FilterModelProcool.swift
@@ -1,9 +1,17 @@
+import UserDomainInterface
+
 protocol FilterStateProtocol {
     var isError: Bool { get }
-    var isSuccess: Bool { get }
+    var isLoading: Bool { get }
+    var isPresentedTechStackAppend: Bool { get }
+    var userRole: UserRoleType { get }
+    var techStacks: [String] { get }
 }
 
 protocol FilterActionProtocol: AnyObject {
     func updateIsError(isError: Bool)
-    func updateIsSuccess(isSuccess: Bool)
+    func updateIsLoading(isLoading: Bool)
+    func updateUserRole(role: UserRoleType)
+    func updateIsPresentedTeckStackAppend(isPresented: Bool)
+    func updateTeckStacks(techStacks: [String])
 }
