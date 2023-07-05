ef7ddb7d81616a91f775d6b75c5a3be3ce6a69ed
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-07-05 14:57:18 +0900

    :sparkles: CaseIterable 추가

--- Projects/Domain/StudentDomain/Interface/Enums/DepartmentType.swift
+++ Projects/Domain/StudentDomain/Interface/Enums/DepartmentType.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // swiftlint: disable identifier_name
-public enum DepartmentType: String, Codable {
+public enum DepartmentType: String, Codable, CaseIterable {
     case software = "SW_DEVELOPMENT"
     case smartIoT = "SMART_IOT_DEVELOPMENT"
     case ai = "AI_DEVELOPMENT"
