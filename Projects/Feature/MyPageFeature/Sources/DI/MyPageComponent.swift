1caee72666a5b7bfda35cfb77e7815351c546184
baegteun <baegteun@gmail.com>
2023-07-20 21:59:31 +0900

    :sparkles: :: [#220] FetchMyProfileUseCase DI -> MyPageComponent

--- Projects/Feature/MyPageFeature/Sources/DI/MyPageComponent.swift
+++ Projects/Feature/MyPageFeature/Sources/DI/MyPageComponent.swift
@@ -2,13 +2,19 @@ import BaseFeature
 import MyPageFeatureInterface
 import NeedleFoundation
 import SwiftUI
+import UserDomainInterface
 
-public protocol MyPageDependency: Dependency {}
+public protocol MyPageDependency: Dependency {
+    var fetchMyProfileUseCase: any FetchMyProfileUseCase { get }
+}
 
 public final class MyPageComponent: Component<MyPageDependency>, MyPageBuildable {
     public func makeView() -> some View {
         let model = MyPageModel()
-        let intent = MyPageIntent(model: model)
+        let intent = MyPageIntent(
+            model: model,
+            fetchMyProfileUseCase: dependency.fetchMyProfileUseCase
+        )
         let container = MVIContainer(
             intent: intent as MyPageIntentProtocol,
             model: model as MyPageStateProtocol,
--- Projects/Feature/MyPageFeature/Sources/Intent/MyPageIntent.swift
+++ Projects/Feature/MyPageFeature/Sources/Intent/MyPageIntent.swift
@@ -1,9 +1,15 @@
 import Foundation
+import UserDomainInterface
 
 final class MyPageIntent:MyPageIntentProtocol {
     private weak var model: (any MyPageActionProtocol)?
+    private let fetchMyProfileUseCase: any FetchMyProfileUseCase
 
-    init(model: any MyPageActionProtocol) {
+    init(
+        model: any MyPageActionProtocol,
+        fetchMyProfileUseCase: any FetchMyProfileUseCase
+    ) {
         self.model = model
+        self.fetchMyProfileUseCase = fetchMyProfileUseCase
     }
 }
