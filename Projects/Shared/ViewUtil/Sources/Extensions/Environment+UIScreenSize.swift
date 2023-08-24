f1baa9a6363d17e32352518f81e7024f32338b72
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-08-24 18:39:46 +0900

    :recycle: [#243] EnvironmentKey / EnvironmentKey 수정

--- Projects/Shared/ViewUtil/Sources/Extensions/Environment+UIScreenSize.swift
+++ Projects/Shared/ViewUtil/Sources/Extensions/Environment+UIScreenSize.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import UIKit
 
 private struct UIScreenSizeKey: EnvironmentKey {
-    static let defaultValue: CGSize = .zero
+    static let defaultValue: CGSize = {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+        return screen.screen.bounds.size
+    }()
 }
 
 public extension EnvironmentValues {
