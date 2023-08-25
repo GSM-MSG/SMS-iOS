085e2c7ccc9ed65188cb60f4c3ba5387cefc59e0
aeongsseu <81360154+HumanIearning@users.noreply.github.com>
2023-08-25 11:09:14 +0900

    Update .github/workflows/CD/Keychain.sh
    Co-authored-by: baegteun <baegteun@gmail.com>

--- .github/workflows/CD/Keychain.sh
+++ .github/workflows/CD/Keychain.sh
@@ -6,7 +6,7 @@ security set-keychain-settings -lut 3600
 security list-keychains
 
 # import cert file to keychain
-security import Tuist/signing/SMS.p12 -k "$KEYCHAIN" -P "$CERTS_EXPORT_PWD" -A
+security import Tuist/Signing/SMS.p12 -k "$KEYCHAIN" -P "$CERTS_EXPORT_PWD" -A
 security set-key-partition-list -S apple-tool:,apple: -s -k "" "$KEYCHAIN"
 
 mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
