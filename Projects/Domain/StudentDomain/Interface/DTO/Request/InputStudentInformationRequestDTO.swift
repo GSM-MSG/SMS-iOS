4c8af54844a707e62113b375f69239543acf7458
uuuunseo <jdbstjoo@gmail.com>
2023-11-22 07:54:55 +0900

    :fire: :: [#309] InputStudentInformationRequestDTO / prizes 삭제

--- Projects/Domain/StudentDomain/Interface/DTO/Request/InputStudentInformationRequestDTO.swift
+++ Projects/Domain/StudentDomain/Interface/DTO/Request/InputStudentInformationRequestDTO.swift
@@ -6,22 +6,19 @@ public struct InputStudentInformationRequestDTO: Encodable {
     public let major: String
     public let profileImgURL: String
     public let techStacks: [String]
-    public let prizes: [Prize]
 
     public init(
         contactEmail: String,
         introduce: String,
         major: String,
         profileImgURL: String,
-        techStacks: [String],
-        prizes: [Prize] = []
+        techStacks: [String]
     ) {
         self.contactEmail = contactEmail
         self.introduce = introduce
         self.major = major
         self.profileImgURL = profileImgURL
         self.techStacks = techStacks
-        self.prizes = prizes
     }
 
     enum CodingKeys: String, CodingKey {
@@ -30,24 +27,5 @@ public struct InputStudentInformationRequestDTO: Encodable {
         case major
         case profileImgURL = "profileImgUrl"
         case techStacks
-        case prizes
-    }
-}
-
-public extension InputStudentInformationRequestDTO {
-    struct Prize: Encodable {
-        public let name: String
-        public let type: String
-        public let date: String
-
-        public init(
-            name: String,
-            type: String,
-            date: String
-        ) {
-            self.name = name
-            self.type = type
-            self.date = date
-        }
     }
 }
