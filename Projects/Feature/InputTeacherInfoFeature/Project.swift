9ffb68897fc6909029aa5703b096525743910145
uuuunseo <jdbstjoo@gmail.com>
2024-01-20 20:46:32 +0900

    :green_heart: :: BaseFeature dependency 추가

--- Projects/Feature/InputTeacherInfoFeature/Project.swift
+++ Projects/Feature/InputTeacherInfoFeature/Project.swift
@@ -6,5 +6,7 @@ let project = Project.makeModule(
     name: ModulePaths.Feature.InputTeacherInfoFeature.rawValue,
     product: .staticLibrary,
     targets: [.interface, .unitTest, .demo],
-    internalDependencies: []
+    internalDependencies: [
+        .Feature.BaseFeature
+    ]
 )
