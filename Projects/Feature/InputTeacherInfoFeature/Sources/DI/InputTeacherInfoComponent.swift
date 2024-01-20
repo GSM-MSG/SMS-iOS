aa12bad35fba278f5451b500b3d7fd915275fdd7
uuuunseo <jdbstjoo@gmail.com>
2024-01-20 16:32:50 +0900

    :lipstick: :: [#315] InputTeacherInfoView / 선생님 정보입력 페이지 UI

--- Projects/Feature/InputTeacherInfoFeature/Sources/DI/InputTeacherInfoComponent.swift
+++ Projects/Feature/InputTeacherInfoFeature/Sources/DI/InputTeacherInfoComponent.swift
@@ -3,20 +3,11 @@ import InputTeacherInfoFeatureInterface
 import NeedleFoundation
 import SwiftUI
 
-public protocol InputTeacherInfoDependency: Dependency {
-    
-}
+public protocol InputTeacherInfoDependency: Dependency {}
 
 public final class InputTeacherInfoComponent: Component<InputTeacherInfoDependency>, InputTeacherInfoBuildable {
 
     public func makeView() -> some View {
-        let model = InputTeacherInfoModel()
-        let intent = InputTeacherInfoIntent(model: model)
-        let container = MVIContainer(
-            intent: intent as InputTeacherInfoIntentProtocol,
-            model: model as InputTeacherInfoStateProtocol,
-            modelChangePublisher: model.objectWillChange
-        )
-        return InputTeacherInfoView(container: container)
+        EmptyView()
     }
 }
--- Projects/Feature/InputTeacherInfoFeature/Sources/Scene/InputTeacherInfoView.swift
+++ Projects/Feature/InputTeacherInfoFeature/Sources/Scene/InputTeacherInfoView.swift
@@ -1,12 +1,58 @@
 import BaseFeature
+import DesignSystem
+import InputInformationBaseFeature
 import SwiftUI
 
 struct InputTeacherInfoView: View {
-    @StateObject var container: MVIContainer<InputTeacherInfoIntentProtocol, InputTeacherInfoStateProtocol>
-    var intent: any InputTeacherInfoIntentProtocol { container.intent }
-    var state: any InputTeacherInfoStateProtocol { container.model }
-    
+
     var body: some View {
-        Text("Hello, World!")
+        SMSNavigationTitleView(title: "정보입력") {
+            VStack(spacing: 0) {
+                HStack(spacing: 4) {
+                    Text("프로필")
+                        .foregroundColor(.sms(.system(.black)))
+
+                    Text("*")
+                        .foregroundColor(.sms(.sub(.s2)))
+
+                    Spacer()
+                }
+                .smsFont(.title1)
+                .padding(.top, 36)
+
+                VStack(spacing: 24) {
+                    inputTeacherInfoTextField(
+                        title: "직함",
+                        placeholder: "직함을 선택해 주세요.",
+                        text: .constant("")
+                    )
+                }
+                .padding(.top, 32)
+
+                CTAButton(text: "완료")
+                    .padding(.top, 32)
+
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+
+    @ViewBuilder
+    func inputTeacherInfoTextField(
+        title: String,
+        placeholder: String,
+        text: Binding<String>
+    ) -> some View {
+        SMSTextField(
+            placeholder,
+            text: text
+        )
+        .disabled(true)
+        .overlay(alignment: .trailing) {
+            SMSIcon(.downChevron)
+                .padding(.trailing, 12)
+        }
+        .titleWrapper(title)
     }
 }
