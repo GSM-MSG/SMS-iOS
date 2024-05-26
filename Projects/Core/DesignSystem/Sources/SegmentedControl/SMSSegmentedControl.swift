b05826e41232953d3debc704dbb0c3fb31b86969
baegteun <baegteun@gmail.com>
2024-05-26 22:42:58 +0900

    :recycle: :: [#336] SMSSegmentedControl / 외부에서 selected option을 제어 가능하도록 변경

--- Projects/Core/DesignSystem/Sources/SegmentedControl/SMSSegmentedControl.swift
+++ Projects/Core/DesignSystem/Sources/SegmentedControl/SMSSegmentedControl.swift
@@ -1,23 +1,26 @@
 import SwiftUI
 
 public struct SMSSegmentedControl: View {
-    @State private var selectedIndex = 0
     @Namespace private var segmentNamespace
     private let options: [String]
+    private let selectedOption: String?
     private let onSelect: (String) -> Void
 
     public init(
         options: [String],
+        selectedOption: String? = nil,
         onSelect: @escaping (String) -> Void
     ) {
         self.options = options
+        self.selectedOption = selectedOption
         self.onSelect = onSelect
     }
 
     public var body: some View {
         HStack(spacing: 0) {
             ForEach(0..<options.count, id: \.self) { index in
-                let isSelected = selectedIndex == index
+                let option = options[index]
+                let isSelected = options[index] == selectedOption
                 SMSText(options[index], font: .title2)
                     .frame(maxWidth: .infinity)
                     .padding(.vertical, 9.5)
@@ -32,12 +35,12 @@ public struct SMSSegmentedControl: View {
                         }
                     }
                     .foregroundStyle(
-                        self.selectedIndex == index ? Color.sms(.system(.white)) : Color.sms(.neutral(.n40))
+                        isSelected ? Color.sms(.system(.white)) : Color.sms(.neutral(.n40))
                     )
                     .padding(4)
                     .buttonWrapper {
                         withAnimation(.interactiveSpring(duration: 0.5)) {
-                            self.selectedIndex = index
+                            onSelect(option)
                         }
                     }
             }
